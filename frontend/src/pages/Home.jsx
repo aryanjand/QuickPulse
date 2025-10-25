47ca5484a917857d35b6ba69aff315331bf85ad8
Towa Quimbayo <towacurtis@hotmail.com>
2025-10-25 14:29:14 -0700

    fixed retro

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
 import "../css/home.css";
+import { useState } from "react";
 
 export default function Home() {
   const navigate = useNavigate();
+  const [prompt, setPrompt] = useState("");
 
   const handleCreateRoom = () => {
     const roomId = crypto.randomUUID();
-    navigate(`/room/${roomId}`);
+    navigate(`/room/${roomId}`, { state: { prompt } });
   };
 
   return (
@@ -16,8 +18,18 @@ export default function Home() {
         <div className="container">
           <h1>QuickPulse</h1>
           <p>Connect people through ideas — one word at a time.</p>
+
+          <div className="prompt-input-container">
+            <input
+              type="text"
+              className="prompt-input"
+              placeholder="Enter your prompt..."
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+            />
+          </div>
           <button onClick={handleCreateRoom} className="create-room-btn">
-            Create a Room
+            Create Room
           </button>
         </div>
       </div>
