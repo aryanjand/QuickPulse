dfffa2b2b6560d6ed98601eb660c0eb307e17d57
Towa Quimbayo <towacurtis@hotmail.com>
2025-10-25 13:25:46 -0700

    added cors to backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = 8000 || process.env.PORT;
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
   console.log(`Backend server running at http://localhost:${PORT}`);
